1d2933d20dcb94f751f4dd0a91340ba73aa2a363
Asimov4 <asimov4@gmail.com>
2016-03-19 13:06:22 -0700

    Adding Parkways blog intent

--- park-events-api.js
+++ park-events-api.js
@@ -1,107 +1,335 @@
-var _feed = require('feedme');
-var _http = require('http');
-var _html_to_text = require('html-to-text');
+/**
+ * This sample demonstrates a simple skill built with the Amazon Alexa Skills Kit.
+ * The Intent Schema, Custom Slots, and Sample Utterances for this skill, as well as
+ * testing instructions are located at http://amzn.to/1LzFrj6
+ *
+ * For additional samples, visit the Alexa Skills Kit Getting Started guide at
+ * http://amzn.to/1LGWsLG
+ */
 
-function get_events(park_name, date, callback) {
-	_http.get('http://www.trumba.com/calendars/parks-recreation.rss', function(res) {
-		var events = [];
-		var parser = new _feed();
-		parser.on('item', function(item) {
-			events.push(parse_event(item));
-		});
-		res.pipe(parser);
-		res.on('end', function() {
-			callback(null, events);
-		});
-	}).on('error', callback);
+// Route the incoming request based on type (LaunchRequest, IntentRequest,
+// etc.) The JSON body of the request is provided in the event parameter.
+exports.handler = function (event, context) {
+    try {
+        console.log("event: " + JSON.stringify(event) + ".\ncontext: " + JSON.stringify(context));
+        console.log("event.session.application.applicationId=" + event.session.application.applicationId);
+
+        /**
+         * Uncomment this if statement and populate with your skill's application ID to
+         * prevent someone else from configuring a skill that sends requests to this function.
+         */
+        /*
+        if (event.session.application.applicationId !== "amzn1.echo-sdk-ams.app.[unique-value-here]") {
+             context.fail("Invalid Application ID");
+        }
+        */
+
+        if (event.session.new) {
+            onSessionStarted({requestId: event.request.requestId}, event.session);
+        }
+
+        if (event.request.type === "LaunchRequest") {
+            onLaunch(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "IntentRequest") {
+            onIntent(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "SessionEndedRequest") {
+            onSessionEnded(event.request, event.session);
+            context.succeed();
+        }
+    } catch (e) {
+        context.fail("Exception: " + e);
+    }
+};
+
+/**
+ * Called when the session starts.
+ */
+function onSessionStarted(sessionStartedRequest, session) {
+    console.log("onSessionStarted requestId=" + sessionStartedRequest.requestId +
+        ", sessionId=" + session.sessionId);
 }
 
-function parse_event(rss_item) {
-	var event_info = {};
-	rss_item['description'].split('<br/><br/>').forEach(function(item) {
-		var text = item.trim();
-		if (text.length > 0) {
-			if (typeof(event_info['time']) === 'undefined' && / \d{1,2}, \d{4}/.test(text)) {
-				var loc_date = parse_loc_date(text);
-				event_info['time'] = loc_date.time;
-				event_info['location'] = loc_date.location;
-			} else if (text.startsWith('<img')) {
-				// Not using this for now
-			} else if (text.startsWith('<b>')) {
-				parse_opt_params(text).forEach(function(opt) {
-					event_info[opt.name] = opt.val;
-				});
-			} else {
-				if (typeof(event_info['description']) !== 'undefined') {
-					throw new Error('Found description twice');
-				}
-				event_info['description'] = _html_to_text.fromString(text).trim();
-			}
-		}
-	});
+/**
+ * Called when the user launches the skill without specifying what they want.
+ */
+function onLaunch(launchRequest, session, callback) {
+    console.log("onLaunch requestId=" + launchRequest.requestId +
+        ", sessionId=" + session.sessionId);
 
-	if (typeof(event_info['name']) !== 'undefined') {
-		throw new Error('Namesapce conflict on member "name"');
-	}
-	event_info['name'] = rss_item['title'];
-
-	if (typeof(event_info['event_link']) !== 'undefined') {
-		throw new Error('Namesapce conflict on member "event_link"');
-	}
-	event_info['event_link'] = rss_item['link'];
-
-	if (typeof(event_info['event_day']) !== 'undefined') {
-		throw new Error('Namesapce conflict on member "event_day"');
-	}
-	event_info['event_day'] = rss_item['category'];
-
-	return event_info;
+    // Dispatch to your skill's launch.
+    getWelcomeResponse(callback);
 }
 
-function parse_loc_date(text) {
-	var entries = text.split('<br/>').map(function(item) {
-		return _html_to_text.fromString(item).trim();
-	});
+/**
+ * Called when the user specifies an intent for this skill.
+ */
+function onIntent(intentRequest, session, callback) {
+    console.log("onIntent requestId=" + intentRequest.requestId +
+        ", sessionId=" + session.sessionId);
 
-	var location, time;
-	if (entries.length === 1) {
-		location = 'N/A';
-		time = entries[0];
-	} else if (entries.length === 2) {
-		location = entries[0];
-		time = entries[1];
-	} else {
-		throw new Error('Unexpected location/date parsing result - ' + entries.length + ' entries');
-	}
+    var intent = intentRequest.intent,
+        intentName = intentRequest.intent.name;
 
-	return {
-		location: location,
-		time: time
-	};
+    // Dispatch to your skill's intent handlers
+    if ("WhatsHappeningInParkIntent" === intentName) {
+        getWhatsHappeningInPark(intent, session, callback);
+    } else if ("ParkwaysBlogPostsIntent" === intentName) {
+        getParkwaysBlogPosts(intent, session, callback);
+    } else if ("AMAZON.YesIntent" === intentName) {
+        readMore(intent, session, callback);
+    } else if ("AMAZON.NoIntent" === intentName) {
+        nextBlogPost(intent, session, callback);
+    } else if ("AMAZON.HelpIntent" === intentName) {
+        getWelcomeResponse(callback);
+    } else if ("AMAZON.StopIntent" === intentName) {
+        getStopResponse(callback);
+    } else if ("AMAZON.CancelIntent" === intentName) {
+        getStopResponse(callback);
+    } else {
+        throw "Invalid intent";
+    }
 }
 
-function parse_opt_params(text) {
-	return text.split('<br/>').map(function(item) {
-		var split_point = item.indexOf(':');
-		if (split_point < 0) {
-			console.log(item);
-			throw new Error('Error parsing - couldn\'t find token');
-		}
-
-		var name = item.substring(0, split_point);
-		name = _html_to_text.fromString(name).trim().toLowerCase().replace(/[\/\s-#]+/g, '_');
-		name = name.replace(/_+$/, '').replace(/^_+/, '');
-
-		var val = item.substring(split_point + 1);
-		val = _html_to_text.fromString(val).trim();
-
-		return {
-			name: name,
-			val: val
-		}
-	});
+/**
+ * Called when the user ends the session.
+ * Is not called when the skill returns shouldEndSession=true.
+ */
+function onSessionEnded(sessionEndedRequest, session) {
+    console.log("onSessionEnded requestId=" + sessionEndedRequest.requestId +
+        ", sessionId=" + session.sessionId);
+    // Add cleanup logic here
 }
 
-module.exports = {
-	get_events: get_events
+// --------------- Functions that control the skill's behavior -----------------------
+
+function getWelcomeResponse(callback) {
+    // If we wanted to initialize the session to have some attributes we could add those here.
+    var sessionAttributes = {};
+    var cardTitle = "Welcome";
+    var speechOutput = "Welcome to the Seattle Parks And Recreation. " +
+        "What would you like to do? You can say hwat is happening in Magnuson Park or read the latest Parkways blog post.";
+    // If the user either does not reply to the welcome message or says something that is not
+    // understood, they will be prompted again with this text.
+    var repromptText = "Please tell me your favorite color by saying, " +
+        "my favorite color is red";
+    var shouldEndSession = false;
+
+    callback(sessionAttributes,
+        buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+}
+
+/**
+ * Sets the color in the session and prepares the speech to reply to the user.
+ */
+ 
+function getWhatsHappeningInPark(intent, session, callback) {
+    var cardTitle = intent.name;
+    var parkSlot = intent.slots.Park;
+    var repromptText = "";
+    var sessionAttributes = {};
+    var shouldEndSession = false;
+    var speechOutput = "I was not able to catch that. What park did you want to go to?";
+
+    var park = parkSlot.value;
+    
+    if (!park) {
+        callback(sessionAttributes,
+                     buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession)); 
+    }
+
+    var https = require('https');
+    
+    var options = {
+        host: 'www.trumba.com',
+        path: '/calendars/volunteer-1.rss?startdate=20160319&search=' + park.replace(/\s?park/i,"")
+    }
+
+    var request = https.request(options, function (res) {
+        var data = '';
+        res.on('data', function (chunk) {
+            data += chunk;
+        });
+        res.on('end', function () {
+            
+            try {
+                var titleRegex = /<title>(.*)<\/title>/ig;
+                var titles = data.match(titleRegex);
+                
+                var eventTitle = titles[2].replace(/<\/?title>/g,"");
+                
+                var descriptionRegex = /<description>(.*)<\/description>/ig;
+                var descriptions = data.match(descriptionRegex);
+                
+                // &lt;br/&gt;Saturday, October 8, 2016, 10am&amp;nbsp;&amp;ndash;&amp;nbsp;2pm &lt;br/&gt;&lt;br/&gt;
+                
+                var eventAddress = descriptions[0].match(/<description>(.*?)&lt;br\/&gt;/ig)[0].replace(/<description>/,"").replace(/&lt;br\/&gt;/g,"");
+                
+                var eventDate = descriptions[0].match(/&lt;br\/&gt;(.*?)&lt;br\/&gt;&lt;br\/&gt;/ig)[0].replace(/\s?&lt;br\/&gt;/g,"").replace(/&amp;nbsp;&amp;ndash;&amp;nbsp;/," to ");
+                
+                speechOutput = "The next event at " + park + " is " + eventTitle + " on " + eventDate + ". The address is " + eventAddress + ".";
+            }
+            catch(err) {
+                speechOutput = "There are no volunteering opportunities planned for " + park + ".";
+            }
+
+            // Setting repromptText to null signifies that we do not want to reprompt the user.
+            // If the user does not respond or says something that is not understood, the session
+            // will end.
+            callback(sessionAttributes,
+                     buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession)); 
+
+        });
+    });
+    request.on('error', function (e) {
+        console.log(e.message);
+    });
+    request.end();
+    
+}
+
+function getParkwaysBlogPosts(intent, session, callback) {
+    var cardTitle = intent.name;
+    var repromptText = "";
+    var sessionAttributes = {};
+    var shouldEndSession = false;
+    var speechOutput = "There was no recent blog post.";
+
+    var https = require('https');
+    
+    var options = {
+        host: 'ajax.googleapis.com',
+        path: '/ajax/services/feed/load?v=2.0&q=http://parkways.seattle.gov/feed/&num=3'
+    }
+
+    var request = https.request(options, function (res) {
+        var data = '';
+        res.on('data', function (chunk) {
+            data += chunk;
+        });
+        res.on('end', function () {
+            
+            blogPosts = JSON.parse(data);
+
+            speechOutput = blogPosts.responseData.feed.entries[0].title + " by " + blogPosts.responseData.feed.entries[0].author + ". Would you like me to read more?";
+            
+            sessionAttributes.blogPosts = blogPosts;
+            sessionAttributes.cursor = 0;
+
+            // Setting repromptText to null signifies that we do not want to reprompt the user.
+            // If the user does not respond or says something that is not understood, the session
+            // will end.
+            callback(sessionAttributes,
+                     buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession)); 
+
+        });
+    });
+    request.on('error', function (e) {
+        console.log(e.message);
+    });
+    request.end();
+}
+
+
+function readMore(intent, session, callback) {
+    var cardTitle = intent.name;
+    var repromptText = "";
+    var sessionAttributes = {};
+    var shouldEndSession = false;
+    var speechOutput = "I did not get that. What did you want to do?";
+    
+    if (session.hasOwnProperty("attributes") && session.attributes.hasOwnProperty("blogPosts")) {
+        speechOutput = session.attributes.blogPosts.responseData.feed.entries[session.attributes.cursor].content.replace(/(<([^>]+)>)/ig,"");
+        
+        speechOutput += "The next blog post is " + session.attributes.blogPosts.responseData.feed.entries[session.attributes.cursor+1].title + " by " + blogPosts.responseData.feed.entries[session.attributes.cursor+1].author + ". Would you like me to read more?";
+    
+        sessionAttributes.blogPosts = session.attributes.blogPosts;
+        sessionAttributes.cursor = session.attributes.cursor+1;
+    }
+    
+    // Setting repromptText to null signifies that we do not want to reprompt the user.
+    // If the user does not respond or says something that is not understood, the session
+    // will end.
+    callback(sessionAttributes,
+             buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession)); 
+
+}
+
+function nextBlogPost(intent, session, callback) {
+    var cardTitle = intent.name;
+    var repromptText = "";
+    var sessionAttributes = {};
+    var shouldEndSession = false;
+    var speechOutput = "I did not get that. What did you want to do?";
+    
+    if (session.hasOwnProperty("attributes") && session.attributes.hasOwnProperty("blogPosts")) {
+        if (session.attributes.cursor > session.attributes.blogPosts.responseData.feed.entries.length-2) {
+            speechOutput = "This was the last blog post. What would you like to do next?";
+        } else {
+            speechOutput = "The next blog post is " + session.attributes.blogPosts.responseData.feed.entries[session.attributes.cursor+1].title + " by " + blogPosts.responseData.feed.entries[session.attributes.cursor+1].author + ". Would you like me to read more?";
+        
+            sessionAttributes.blogPosts = session.attributes.blogPosts;
+            sessionAttributes.cursor = session.attributes.cursor+1;
+        }
+    }
+    
+    // Setting repromptText to null signifies that we do not want to reprompt the user.
+    // If the user does not respond or says something that is not understood, the session
+    // will end.
+    callback(sessionAttributes,
+             buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession)); 
+
+}
+
+
+function getStopResponse(callback) {
+    var repromptText = null;
+    var cardTitle = "Goodbye";
+    var shouldEndSession = true;
+    var speechOutput = "Thanks for checking in with Seattle Parks. Enjoy your next park visit!";
+    var sessionAttributes = {};
+
+    // Setting repromptText to null signifies that we do not want to reprompt the user.
+    // If the user does not respond or says something that is not understood, the session
+    // will end.
+    callback(sessionAttributes,
+             buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+}
+
+// --------------- Helpers that build all of the responses -----------------------
+
+function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        card: {
+            type: "Simple",
+            title: "SessionSpeechlet - " + title,
+            content: "SessionSpeechlet - " + output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildResponse(sessionAttributes, speechletResponse) {
+    return {
+        version: "1.0",
+        sessionAttributes: sessionAttributes,
+        response: speechletResponse
+    };
 }
