5d9931cf5307a91dfdf3ea75cd7b34cfd449abb6
Katrina Ezis <katrinaezis@gmail.com>
2016-03-19 16:46:54 -0700

    css and js

--- static/js/main.js
+++ static/js/main.js
@@ -1,26 +1,24 @@
 $(document).ready(function() {
-	// $("#events").append("<div> hello </div>");
+    var url = "data/parks.json";
+    $.ajax({
+        type: 'GET',
+        url: url,
+        async: false,
+        contentType: "application/json",
+        dataType: 'json',
+        success: function (data) {
+            eventData = data.events;
+            $.each(eventData, function(i, val) {
+                var date = $.format.date(val.dtstamp, "MM/dd");
+                var time = $.format.date(val.dtstart, "h:mm a");
+            	console.log(val);
+            	//$("#events").append("<div> hello </div>");
+    			$("#events").append("<div class='eventResult container'><h3>" + date + " - " + val.summary + "<img class='chev' src='../img/noun_128420_cc.svg'></h3><h5>" + val.location + " at " + time +"</h5></div>");
+    			
+    		});
+        },
+        error: function (e) {
+            alert('error');
+        }
+    });
 });
-
-var url = "data/parks.json";
-$.ajax({
-    type: 'GET',
-    url: url,
-    async: false,
-    contentType: "application/json",
-    dataType: 'json',
-    success: function (data) {
-        eventData = data.events;
-        $.each(eventData, function(i, val) {
-        	console.log(val.categories);
-        	//$("#events").append("<div> hello </div>");
-			$("#events").append("<div class='eventResult'>" + val.categories + "</div>");
-			
-		});
-    },
-    error: function (e) {
-        alert('error');
-    }
-});
-
-
