b7efba83e8906ff541118157e20f1d8fbed23b5a
Asimov4 <asimov4@gmail.com>
2016-03-19 10:37:57 -0700

    Adding Alexa Skill Lambda function
    First commit of the Seattle Parks Lambda function

---
+++ seattleParksLambda.js
@@ -0,0 +1,246 @@
+/**
+ * This sample demonstrates a simple skill built with the Amazon Alexa Skills Kit.
+ * The Intent Schema, Custom Slots, and Sample Utterances for this skill, as well as
+ * testing instructions are located at http://amzn.to/1LzFrj6
+ *
+ * For additional samples, visit the Alexa Skills Kit Getting Started guide at
+ * http://amzn.to/1LGWsLG
+ */
+
+// Route the incoming request based on type (LaunchRequest, IntentRequest,
+// etc.) The JSON body of the request is provided in the event parameter.
+exports.handler = function (event, context) {
+    try {
+        console.log("event: " + JSON.stringify(event) + ".\ncontext: " + JSON.stringify(context));
+        console.log("event.session.application.applicationId=" + event.session.application.applicationId);
+
+        /**
+         * Uncomment this if statement and populate with your skill's application ID to
+         * prevent someone else from configuring a skill that sends requests to this function.
+         */
+        /*
+        if (event.session.application.applicationId !== "amzn1.echo-sdk-ams.app.[unique-value-here]") {
+             context.fail("Invalid Application ID");
+        }
+        */
+
+        if (event.session.new) {
+            onSessionStarted({requestId: event.request.requestId}, event.session);
+        }
+
+        if (event.request.type === "LaunchRequest") {
+            onLaunch(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "IntentRequest") {
+            onIntent(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "SessionEndedRequest") {
+            onSessionEnded(event.request, event.session);
+            context.succeed();
+        }
+    } catch (e) {
+        context.fail("Exception: " + e);
+    }
+};
+
+/**
+ * Called when the session starts.
+ */
+function onSessionStarted(sessionStartedRequest, session) {
+    console.log("onSessionStarted requestId=" + sessionStartedRequest.requestId +
+        ", sessionId=" + session.sessionId);
+}
+
+/**
+ * Called when the user launches the skill without specifying what they want.
+ */
+function onLaunch(launchRequest, session, callback) {
+    console.log("onLaunch requestId=" + launchRequest.requestId +
+        ", sessionId=" + session.sessionId);
+
+    // Dispatch to your skill's launch.
+    getWelcomeResponse(callback);
+}
+
+/**
+ * Called when the user specifies an intent for this skill.
+ */
+function onIntent(intentRequest, session, callback) {
+    console.log("onIntent requestId=" + intentRequest.requestId +
+        ", sessionId=" + session.sessionId);
+
+    var intent = intentRequest.intent,
+        intentName = intentRequest.intent.name;
+
+    // Dispatch to your skill's intent handlers
+    if ("WhatsHappeningInParkIntent" === intentName) {
+        getWhatsHappeningInPark(intent, session, callback);
+    } else if ("WhatsMyColorIntent" === intentName) {
+        getColorFromSession(intent, session, callback);
+    } else if ("AMAZON.HelpIntent" === intentName) {
+        getWelcomeResponse(callback);
+    } else {
+        throw "Invalid intent";
+    }
+}
+
+/**
+ * Called when the user ends the session.
+ * Is not called when the skill returns shouldEndSession=true.
+ */
+function onSessionEnded(sessionEndedRequest, session) {
+    console.log("onSessionEnded requestId=" + sessionEndedRequest.requestId +
+        ", sessionId=" + session.sessionId);
+    // Add cleanup logic here
+}
+
+// --------------- Functions that control the skill's behavior -----------------------
+
+function getWelcomeResponse(callback) {
+    // If we wanted to initialize the session to have some attributes we could add those here.
+    var sessionAttributes = {};
+    var cardTitle = "Welcome";
+    var speechOutput = "Welcome to the Seattle Park Skill. " +
+        "What park do you want to go to?";
+    // If the user either does not reply to the welcome message or says something that is not
+    // understood, they will be prompted again with this text.
+    var repromptText = "Please tell me your favorite color by saying, " +
+        "my favorite color is red";
+    var shouldEndSession = false;
+
+    callback(sessionAttributes,
+        buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+}
+
+/**
+ * Sets the color in the session and prepares the speech to reply to the user.
+ */
+ 
+function getWhatsHappeningInPark(intent, session, callback) {
+    var cardTitle = intent.name;
+    var parkSlot = intent.slots.Park;
+    var repromptText = "";
+    var sessionAttributes = {};
+    var shouldEndSession = false;
+    var speechOutput = "";
+
+    var park = parkSlot.value;
+
+    var https = require('https');
+    
+    var options = {
+        host: 'www.trumba.com',
+        path: '/calendars/volunteer-1.rss?startdate=20150319&search=' + park.replace(/\s?park/i,"")
+    }
+
+    var request = https.request(options, function (res) {
+        var data = '';
+        res.on('data', function (chunk) {
+            data += chunk;
+        });
+        res.on('end', function () {
+            
+            try {
+                var titleRegex = /<title>(.*)<\/title>/ig;
+                var titles = data.match(titleRegex);
+                
+                var eventTitle = titles[2].replace(/<\/?title>/g,"");
+                
+                var descriptionRegex = /<description>(.*)<\/description>/ig;
+                var descriptions = data.match(descriptionRegex);
+                
+                // &lt;br/&gt;Saturday, October 8, 2016, 10am&amp;nbsp;&amp;ndash;&amp;nbsp;2pm &lt;br/&gt;&lt;br/&gt;
+                
+                var eventAddress = descriptions[0].match(/<description>(.*?)&lt;br\/&gt;/ig)[0].replace(/<description>/,"").replace(/&lt;br\/&gt;/g,"");
+                
+                var eventDate = descriptions[0].match(/&lt;br\/&gt;(.*?)&lt;br\/&gt;&lt;br\/&gt;/ig)[0].replace(/\s?&lt;br\/&gt;/g,"").replace(/&amp;nbsp;&amp;ndash;&amp;nbsp;/," to ");
+                
+                speechOutput = "The next event at " + park + " is " + eventTitle + " on " + eventDate + ". The address is " + eventAddress + ".";
+            }
+            catch(err) {
+                speechOutput = "There are no volunteering opportunities planned for " + park + ".";
+            }
+
+            // Setting repromptText to null signifies that we do not want to reprompt the user.
+            // If the user does not respond or says something that is not understood, the session
+            // will end.
+            callback(sessionAttributes,
+                     buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession)); 
+
+        });
+    });
+    request.on('error', function (e) {
+        console.log(e.message);
+    });
+    request.end();
+    
+}
+
+function createFavoriteColorAttributes(favoriteColor) {
+    return {
+        favoriteColor: favoriteColor
+    };
+}
+
+function getColorFromSession(intent, session, callback) {
+    var favoriteColor;
+    var repromptText = null;
+    var sessionAttributes = {};
+    var shouldEndSession = false;
+    var speechOutput = "";
+
+    if (session.attributes) {
+        favoriteColor = session.attributes.favoriteColor;
+    }
+
+    if (favoriteColor) {
+        speechOutput = "Your favorite color is " + favoriteColor + ". Goodbye.";
+        shouldEndSession = true;
+    } else {
+        speechOutput = "I'm not sure what your favorite color is, you can say, my favorite color " +
+            " is red";
+    }
+
+    // Setting repromptText to null signifies that we do not want to reprompt the user.
+    // If the user does not respond or says something that is not understood, the session
+    // will end.
+    callback(sessionAttributes,
+         buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
+}
+
+// --------------- Helpers that build all of the responses -----------------------
+
+function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        card: {
+            type: "Simple",
+            title: "SessionSpeechlet - " + title,
+            content: "SessionSpeechlet - " + output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildResponse(sessionAttributes, speechletResponse) {
+    return {
+        version: "1.0",
+        sessionAttributes: sessionAttributes,
+        response: speechletResponse
+    };
+}
