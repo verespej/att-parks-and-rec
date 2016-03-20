b228d368f84ce4205524d07e9b47e940dcd02ab8
Katrina Ezis <katrinaezis@gmail.com>
2016-03-19 17:10:39 -0700

    whoop we done

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,5 @@
+var myArray = [];
+
 $(document).ready(function() {
     var url = "data/parks.json";
     $.ajax({
@@ -7,13 +9,16 @@ $(document).ready(function() {
         contentType: "application/json",
         dataType: 'json',
         success: function (data) {
+            data.events.sort(function(first, second) {
+                return first.dtstamp.localeCompare(second.dtstamp);
+            });
             eventData = data.events;
             $.each(eventData, function(i, val) {
                 var date = $.format.date(val.dtstamp, "MM/dd");
                 var time = $.format.date(val.dtstart, "h:mm a");
             	console.log(val);
             	//$("#events").append("<div> hello </div>");
-    			$("#events").append("<div class='eventResult container'><h3>" + date + " - " + val.summary + "<img class='chev' src='../img/noun_128420_cc.svg'></h3><h5>" + val.location + " at " + time +"</h5></div>");
+    			$("#events").append("<a href='" + val.url + "'><div class='eventResult container'><h3>" + date + " - " + val.summary + "</h3><img class='chev' src='../img/noun_128420_cc.svg'><h5>" + val.location + " at " + time +"</h5></div></a>");
     			
     		});
         },
@@ -22,3 +27,9 @@ $(document).ready(function() {
         }
     });
 });
+
+// function array.sort(a, b) {
+//     return new Date(b.dtstamp) - new Date(a.dtstamp);
+// }
+
+
